0f3887c9e855e255b163d40d889220b8cb6ad5eb
Andy Chen <totallynotandychen@gmail.com>
2022-07-22 21:48:28 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+#initiate the game
+pygame.init()
+
+#screen
+screen = pygame.display.set_mode((800, 600))
+
+#title
+pygame.display.set_caption("Stop The Wolf")
+icon = pygame.image.load("assets/icon.png")
+pygame.display.set_icon(icon)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        screen.fill((0, 0, 0))
+        pygame.display.update()
